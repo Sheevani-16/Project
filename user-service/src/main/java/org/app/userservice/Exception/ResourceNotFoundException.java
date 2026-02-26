eb628f3d76c36ed5b03514205270deba20e3cda3
PavankumarJolad <pavankumarjolad@gmail.com>
2026-02-26 17:13:59 +0530

    Api gateway implemented

--- user-service/src/main/java/org/app/userservice/Exception/ResourceNotFoundException.java
+++ user-service/src/main/java/org/app/userservice/Exception/ResourceNotFoundException.java
@@ -1,6 +1,5 @@
 package org.app.userservice.Exception;
 public class ResourceNotFoundException extends RuntimeException {
-
     public ResourceNotFoundException(String message) {
         super(message);
     }
